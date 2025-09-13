20510b8f420e811f9e3b357cf45f8c00a11cd462
Max Filippov <jcmvbkbc@gmail.com>
2025-09-13 01:45:43 -0700

    esp_wifi: fix .get_tx_power()
    Fix get_tx_power prototype on rebase to v6.16
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- drivers/net/wireless/espressif/esp32-ng/esp_cfg80211.c
+++ drivers/net/wireless/espressif/esp32-ng/esp_cfg80211.c
@@ -579,6 +579,8 @@ static int esp_cfg80211_get_station(struct wiphy *wiphy, struct wireless_dev *wd
 
 static int esp_cfg80211_get_tx_power(struct wiphy *wiphy,
 				     struct wireless_dev *wdev,
+				     int radio_idx,
+				     unsigned int link_id,
 				     int *dbm)
 {
 	struct esp_wifi_device *priv = NULL;
